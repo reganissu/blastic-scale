7c6d8a68905f54671cefe4459fffef95deeb7c44
Lorenzo Pistone <blaffablaffa@gmail.com>
2024-09-06 20:00:21 +0200

    implement main with basic CLI command (ping/pong)

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,18 @@
+#include "SerialCliTask.h"
 #include <Arduino.h>
 #include <Arduino_FreeRTOS.h>
 
-void spam(void *warmup);
+void loop() {}
 
-void serial_read(void *warmup);
+static void cliPing(const String &params) { Serial.print(F("pong\n")); }
 
-void print_millis(void *warmup);
+static constexpr const CliCallback cliCallbacks[]{CliCallback("ping", cliPing), CliCallback()};
 
-void print_micros(void *warmup);
-
-void loop(){}
+SerialCliTask cliTask(Serial, cliCallbacks);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial);
-  Serial.println("boot!");
-
-  randomSeed(analogRead(0));
-
-  xTaskCreate(spam, "spam", configMINIMAL_STACK_SIZE, reinterpret_cast<void*>(random(20)), tskIDLE_PRIORITY + 1, nullptr);
-  xTaskCreate(serial_read, "serial_read", configMINIMAL_STACK_SIZE, reinterpret_cast<void*>(random(20)), tskIDLE_PRIORITY + 1, nullptr);
-  xTaskCreate(print_millis, "print_millis", configMINIMAL_STACK_SIZE, reinterpret_cast<void*>(random(20)), tskIDLE_PRIORITY + 1, nullptr);
-  xTaskCreate(print_micros, "print_micros ", configMINIMAL_STACK_SIZE, reinterpret_cast<void*>(random(20)), tskIDLE_PRIORITY + 1, nullptr);
-
+  cliTask.create(cliCallbacks);
   vTaskStartScheduler();
-  for(;;);
+
+  for (;;);
 }
