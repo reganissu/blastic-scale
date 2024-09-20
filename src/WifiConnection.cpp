7c529171e2ae22d8702dab4b179b42283f769555
Lorenzo Pistone <blaffablaffa@gmail.com>
2024-09-20 18:57:35 +0200

    avoid potential deadlock with debug in wifi

--- src/WifiConnection.cpp
+++ src/WifiConnection.cpp
@@ -11,10 +11,7 @@ static void timeoutDisconnect() {
     {
       MWiFi wifi;
       if (!lastUnusedTime || millis() - lastUnusedTime < timeoutSec * 1000) continue;
-      MSerial serial(debug);
-      if (serial.taken()) modem.debug(*serial, 2);
       wifi->end();
-      modem.noDebug();
       lastUnusedTime = 0;
     }
     MSerial()->print("wifi: disconneted\n");
@@ -22,15 +19,12 @@ static void timeoutDisconnect() {
 }
 
 WifiConnection::WifiConnection(const EEPROMConfig &config) : util::Mutexed<WiFi>() {
-  MWiFi &wifi = *this;
-  MSerial serial(debug);
-  if (serial.taken()) modem.debug(*serial, 2);
+  auto &wifi = *this;
   configASSERT(!strcmp(wifi->firmwareVersion(), WIFI_FIRMWARE_LATEST_VERSION));
   timeoutSec = config.timeoutSec;
   if (*this && !strncmp(wifi->SSID(), config.ssid, sizeof(config.ssid))) return;
   wifi->end();
   wifi->begin(config.ssid, strnlen(config.password, sizeof(config.password)) ? config.password : nullptr);
-  modem.noDebug();
 }
 
 WifiConnection::operator bool() const { return (*this)->status() == WL_CONNECTED; }
