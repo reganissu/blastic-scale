f29ee0f3b5d3d8c1abf22c554101918ad8dfadab
Lorenzo Pistone <blaffablaffa@gmail.com>
2024-10-22 21:16:25 +0200

    tidy up

--- scripts/arduino-cli/monitor.sh
+++ scripts/arduino-cli/monitor.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 . ./scripts/arduino-cli/default-port.sh
 
-arduino-cli monitor --fqbn arduino:renesas_uno:unor4wifi -p "$BLASTIC_SCALE_PORT" --config baudrate=115200
+arduino-cli monitor --fqbn arduino:renesas_uno:unor4wifi -p "$BLASTIC_SCALE_PORT" --config baudrate=115200 "${@}"
--- scripts/arduino-cli/upload.sh
+++ scripts/arduino-cli/upload.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 
 . ./scripts/arduino-cli/default-port.sh
 
-arduino-cli upload --fqbn arduino:renesas_uno:unor4wifi -i .arduino-cli-build/blastic-scale.ino.bin -p "$BLASTIC_SCALE_PORT"
+arduino-cli upload --fqbn arduino:renesas_uno:unor4wifi -i .arduino-cli-build/blastic-scale.ino.bin -p "$BLASTIC_SCALE_PORT" "${@}"
