ed51e3ad2c70e25962ad7c587957181e8c1f2b28
Lorenzo Pistone <blaffablaffa@gmail.com>
2024-10-26 16:18:55 +0200

    edsp-idf-trust  -> esp-idf-trust

--- scripts/uno-r4-wifi-usb-bridge-add-mitmproxy-ca.sh
+++ scripts/uno-r4-wifi-usb-bridge-add-mitmproxy-ca.sh
@@ -12,11 +12,11 @@ cp -v "$firmware" "$firmware".orig
 
 cat deps/uno-r4-wifi-usb-bridge/certificates/cacrt_all.pem ~/.mitmproxy/mitmproxy-ca-cert.cer > uno-r4-wifi-usb-bridge-trust-with-mitmproxy.pem
 python deps/uno-r4-wifi-usb-bridge/certificates/gen_crt_bundle.py -i uno-r4-wifi-usb-bridge-trust-with-mitmproxy.pem
-mv x509_crt_bundle uno-r4-wifi-usb-bridge-trust-with-mitmproxy.edsp-idf-trust
-if [[ $(stat --printf="%s" uno-r4-wifi-usb-bridge-trust-with-mitmproxy.edsp-idf-trust) -gt 282624 ]]; then
-  echo 'uno-r4-wifi-usb-bridge-trust-with-mitmproxy.edsp-idf-trust is larger than certs map size (0x045000)!' 1>&2
+mv x509_crt_bundle uno-r4-wifi-usb-bridge-trust-with-mitmproxy.esp-idf-trust
+if [[ $(stat --printf="%s" uno-r4-wifi-usb-bridge-trust-with-mitmproxy.esp-idf-trust) -gt 282624 ]]; then
+  echo 'uno-r4-wifi-usb-bridge-trust-with-mitmproxy.esp-idf-trust is larger than certs map size (0x045000)!' 1>&2
   exit 1;
 fi
-dd if=uno-r4-wifi-usb-bridge-trust-with-mitmproxy.edsp-idf-trust of="$firmware" bs=4096 seek=11 conv=notrunc
+dd if=uno-r4-wifi-usb-bridge-trust-with-mitmproxy.esp-idf-trust of="$firmware" bs=4096 seek=11 conv=notrunc
 
 echo "mitmproxy ca has been added to the trust anchor, now flash $firmware on your board"
